7bb38a23a0a92a9af21f5f8fdbb92d3755e2c8b2
DavidYong <yongcheeho@gmail.com>
2024-06-05 12:25:35 +0800

    Added bot running card

---
+++ src/routes/bots/Running.svelte
@@ -0,0 +1,108 @@
+<script>
+    
+</script>
+
+<div class="p-3 border card">
+    <div class="flex items-center gap-2 mb-3">
+        <div class="w-7 h-7">
+            <img src="/img/coin-icons/eth.png">
+        </div>
+       
+        <div class="flex flex-col">
+            <div class="font-semibold">
+                ETH/USDT Grid Trading
+            </div>
+            <div class="text-xs text-gray-400/80">
+                --(2024/05/28 15:28:24 Created)
+            </div> 
+        </div>
+        
+    </div>
+
+    <div class="flex justify-between w-full mb-3 overflow-hidden border border-green-500 rounded-xl">
+        <div class="flex flex-col w-1/2 gap-1 p-3">
+            <div class="text-xs">
+                Investment (USDT)
+            </div>
+            <div class="text-sm font-semibold">
+                16.72
+            </div>
+        </div>
+        
+        <div class="flex flex-col w-2/3 gap-1 p-3 text-white bg-green-500 trapezium-bg">
+            <div class="text-xs text-right">
+                Current Profit (USDT)
+            </div>
+            <div class="text-sm font-semibold text-right">
+                +0.00396(+0.02%)
+            </div>
+        </div>
+    </div>
+
+    <div class="flex w-full px-3 mb-3">
+        <div class="w-1/2 text-left">
+            <div class="text-xs text-gray-400/80">
+                Floating Profit USDT
+            </div>
+            <div class="text-sm font-semibold text-green-500">
+                +0.000
+            </div>
+            <div class="text-sm font-semibold text-green-500">
+                +0.00%
+            </div>
+        </div>
+    
+        <div class="w-1/2 text-right">
+            <div class="text-xs text-gray-400/80">
+                Grid Profit USDT
+            </div>
+            <div class="text-sm font-semibold text-green-500">
+                +0.000
+            </div>
+            <div class="text-sm font-semibold text-green-500">
+                +0.00%
+            </div>
+        </div>
+    </div>
+
+    <div class="flex w-full px-3 mb-3">
+        <div class="w-1/2 text-left">
+            <div class="text-xs text-gray-400/80">
+                Price range USDT
+            </div>
+            <div class="text-sm font-semibold ">
+                3,345.31 - 4533.18
+            </div>
+            <div class="text-sm font-semibold ">
+                (50 grids)
+            </div>
+        </div>
+    
+        <div class="w-1/2 text-right">
+            <div class="text-xs text-gray-400/80">
+                Current Price USDT
+            </div>
+            <div class="text-sm font-semibold ">
+                3850.01
+            </div>
+        </div>
+    </div>
+   
+    <div class="flex gap-2 text-sm">
+        <button class="flex justify-center flex-1 py-1 text-white bg-red-500 rounded-md">
+            End Bot
+        </button>
+        <a href='/bots/details' class="flex justify-center flex-1 p-1 border rounded-md">
+            Details
+        </a>
+    </div>
+    
+</div>
+
+<style>.trapezium-bg {
+    background-color: #22c55e; /* green-500 color */
+    position: relative;
+    clip-path: polygon(25% 0%, 100% 0%, 100% 100%, 0% 100%);
+    /* Adjust the first point (25% 0%) to get the desired trapezium effect */
+}
+</style>
---
+++ src/routes/bots/details/+page.svelte
@@ -0,0 +1,33 @@
+<script lang='ts'>
+	import Icon from '@iconify/svelte';
+    import BackHeader from '$lib/components/BackHeader.svelte';
+    import { TabGroup, Tab, TabAnchor } from '@skeletonlabs/skeleton';
+
+    let tabSet = 0
+   
+</script>
+<BackHeader path="/bots" layout="flex items-center bg-surface-100 pb-2">
+    <div class="flex justify-start flex-grow font-semibold flex-2 h3">Details</div>
+</BackHeader>
+
+<TabGroup border="" justify="justify-center" class="flex flex-col flex-grow w-full h-full font-semibold" active="border-primary-500 border-b-2 text-primary-500">
+    <div class="flex flex-grow">
+        <Tab bind:group={tabSet} name="tab1" value={0} class="flex justify-center flex-1 flex-grow w-1/2">
+            <span>Current Plan</span>
+        </Tab>
+        <Tab bind:group={tabSet} name="tab2" value={1} class="flex justify-center flex-1 flex-grow w-1/2">History</Tab>
+    </div>
+
+    <!-- Tab Panels --->
+        <svelte:fragment slot="panel">
+            <div class="flex flex-col flex-grow">
+                {#if tabSet === 0}
+          
+        {:else if tabSet === 1}
+          
+        {/if}
+            </div>
+        
+    </svelte:fragment>
+    
+</TabGroup>
